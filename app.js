aaac3beaca13daa5b4728837e3b271885e22aae9
HabibaRasool123 <rasoolhabiba00@gmail.com>
2026-06-04 08:38:09 +0500

    Initial DevOps project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+let visitors = 0;
+
+app.get('/', (req, res) => {
+    visitors++;
+
+    res.send(`
+        <h1>DevOps Project</h1>
+        <p><strong>Timestamp:</strong> ${new Date()}</p>
+        <p><strong>Container ID:</strong> ${os.hostname()}</p>
+        <p><strong>Visitors:</strong> ${visitors}</p>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
